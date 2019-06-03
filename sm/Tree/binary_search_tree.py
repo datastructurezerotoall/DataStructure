ac3d88033f17d838861704666f2859a51fc99c14
Ronalmoo <ydaniel0826@gmail.com>
2019-06-03 13:19:54 +0900

    # update Tree(binary_tree)

--- sm/Tree/binary_search_tree.py
+++ sm/Tree/binary_search_tree.py
@@ -49,4 +49,66 @@ class BST:
                 cur = cur.right
         return cur
 
+    def __remove_recursion(self, cur, target):
+        if not cur:
+            return None, None
+        elif target < cur.data:
+            cur.left, rem_node = self.__remove_recursion(cur.left, target)
+        elif target > cur.data:
+            cur.right, rem_node = self.__remove_recursion(cur.right, target)
+        else:
+            if not cur.left and not cur.right:
+                rem_node = cur
+                cur = None
+            elif not cur.right:
+                rem_node = cur
+                cur = cur.left
+            elif not cur.left:
+                rem_node = cur
+                cur = cur.right
+            else:
+                replace = cur.left
+                while replace.right:
+                    replace = replace.right
+                cur.data, replace.data = replace.data, cur.data
+                cur.left, rem_node = self.__remove_recursion(
+                    cur.left, replace.data)
+        return cur, rem_node
 
+    def remove(self, target):
+        self.root, removed_node = self.__remove_recursion(self.root, target)
+        if removed_node:
+            removed_node.left = removed_node.right = None
+        return removed_node
+
+
+if __name__ == "__main__":
+    print('*' * 100)
+    bst = BST()
+
+    bst.insert(6)
+    bst.insert(3)
+    bst.insert(2)
+    bst.insert(4)
+    bst.insert(5)
+    bst.insert(8)
+    bst.insert(10)
+    bst.insert(9)
+    bst.insert(11)
+
+    def f(x): return print(x.data, end='  ')
+
+    bst.preorder_traverse(bst.get_root(), f)
+    print()
+
+    print('searched data : {}'.format(bst.search(8).data))
+
+    # bst.remove(9)
+    # bst.remove(8)
+    # bst.remove(6)
+
+    print(bst.remove(15))
+
+    bst.preorder_traverse(bst.get_root(), f)
+    print()
+    print('*' * 100)
